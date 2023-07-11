458c4c168e8d6419e1989338bdda1ea2e0ac5acb
xingyuan <huang.xingyuan@outlook.com>
2023-07-11 10:02:52 +0800

    增加乘车人列表查询接口，只能查看当前全员自己添加的乘客

--- member/src/main/java/com/xingyuan/train/member/controller/PassengerController.java
+++ member/src/main/java/com/xingyuan/train/member/controller/PassengerController.java
@@ -1,18 +1,20 @@
 package com.xingyuan.train.member.controller;
 
+import com.xingyuan.train.common.context.LoginMemberContext;
 import com.xingyuan.train.common.resp.CommonResp;
 import com.xingyuan.train.member.req.MemberLoginReq;
 import com.xingyuan.train.member.req.MemberSendCodeReq;
+import com.xingyuan.train.member.req.PassengerQueryReq;
 import com.xingyuan.train.member.req.PassengerSaveReq;
 import com.xingyuan.train.member.resp.MemberLoginResp;
+import com.xingyuan.train.member.resp.PassengerQueryResp;
 import com.xingyuan.train.member.service.MemberService;
 import com.xingyuan.train.member.service.PassengerService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author Xingyuan Huang
@@ -28,6 +30,13 @@ public class PassengerController {
     public CommonResp<Object> save(@Valid @RequestBody PassengerSaveReq req){
         passengerService.save(req);
         return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<List<PassengerQueryResp>> queryList(@Valid PassengerQueryReq req){
+        req.setMemberId(LoginMemberContext.getId());
+        List<PassengerQueryResp> respList = passengerService.queryList(req);
+        return new CommonResp<>(respList);
     }
 
 }
---
+++ member/src/main/java/com/xingyuan/train/member/req/PassengerQueryReq.java
@@ -0,0 +1,13 @@
+package com.xingyuan.train.member.req;
+
+import lombok.Data;
+
+/**
+ * @author Xingyuan Huang
+ * @since 2023/7/11 9:49
+ */
+
+@Data
+public class PassengerQueryReq {
+    private Long memberId;
+}
