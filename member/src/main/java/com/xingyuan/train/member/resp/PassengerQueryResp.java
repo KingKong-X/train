8fc98512b19d347892b106e1a5bdde4a00b61020
xingyuan <huang.xingyuan@outlook.com>
2023-07-11 12:27:17 +0800

    解决前后端交互Long类型精度丢失的问题

--- member/src/main/java/com/xingyuan/train/member/resp/PassengerQueryResp.java
+++ member/src/main/java/com/xingyuan/train/member/resp/PassengerQueryResp.java
@@ -1,6 +1,8 @@
 package com.xingyuan.train.member.resp;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.xingyuan.train.common.resp.PageResp;
 import lombok.Data;
 
@@ -8,8 +10,10 @@ import java.util.Date;
 
 @Data
 public class PassengerQueryResp {
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
 
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long memberId;
 
     private String name;
