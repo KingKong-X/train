4c2bc8dadefeeab8e16877b614a43c942661a0a8
xingyuan <huang.xingyuan@outlook.com>
2023-07-12 13:42:08 +0800

    为火车座位页面增加车次查询条件

--- business/src/main/java/com/xingyuan/train/business/req/TrainSeatQueryReq.java
+++ business/src/main/java/com/xingyuan/train/business/req/TrainSeatQueryReq.java
@@ -1,12 +1,11 @@
 package com.xingyuan.train.business.req;
 
 import com.xingyuan.train.common.req.PageReq;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class TrainSeatQueryReq extends PageReq {
-
-    @Override
-    public String toString() {
-        return "TrainSeatQueryReq{" +
-                "} " + super.toString();
-    }
+    private String trainCode;
 }
