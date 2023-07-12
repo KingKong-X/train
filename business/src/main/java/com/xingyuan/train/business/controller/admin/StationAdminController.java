2cd57b78a41c0b859ebeefdcb20f3c1f4b69d376
xingyuan <huang.xingyuan@outlook.com>
2023-07-12 13:07:21 +0800

    车站下拉框开发

--- business/src/main/java/com/xingyuan/train/business/controller/admin/StationAdminController.java
+++ business/src/main/java/com/xingyuan/train/business/controller/admin/StationAdminController.java
@@ -10,6 +10,8 @@ import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 /**
  * @author Xingyuan Huang
  * @since 2023/7/11 20:27
@@ -36,5 +38,11 @@ public class StationAdminController {
     public CommonResp<Object> delete(@PathVariable Long id) {
         stationService.delete(id);
         return new CommonResp<>();
+    }
+
+    @GetMapping("/query-all")
+    public CommonResp<List<StationQueryResp>> queryList() {
+        List<StationQueryResp> list = stationService.queryAll();
+        return new CommonResp<>(list);
     }
 }
